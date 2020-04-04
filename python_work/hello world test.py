a0e37b3b5fb7218e9cab767c13ac34bffb323e19
IoTdev-Jun-CN <88253426@qq.com>
2020-04-04 21:46:45 +0800

    add python

---
+++ python_work/hello world test.py
@@ -0,0 +1,2 @@
+message = "veve xue"
+print(message)
---
+++ python_work/name.py
@@ -0,0 +1,8 @@
+first_name ="veve"
+last_name ="xue"
+full_name =first_name+" "+last_name
+print(full_name.title())
+print(full_name.upper())
+print(full_name.lower())
+print(full_name)
+print("Hello,"+full_name.title())
